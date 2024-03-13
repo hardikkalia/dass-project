7abc471d9a4c178fd2d8a39ca89be4abd4498751
argamas23 <127509973+argamas23@users.noreply.github.com>
2024-03-13 12:16:14 -0700

    Password Eye working

---
+++ code/cart_genie/lib/common/widgets/custom_hidefield.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:cart_genie/constants/global_variables.dart';
+
+class CustomHideField extends StatefulWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final TextInputType keyboardType;
+  final String? Function(String?)? validator;
+  final Widget? suffixIcon;
+  final VoidCallback? onSuffixIconPressed;
+
+  const CustomHideField({
+    Key? key,
+    required this.controller,
+    required this.hintText,
+    required this.keyboardType,
+    this.validator,
+    this.suffixIcon,
+    this.onSuffixIconPressed,
+  }) : super(key: key);
+
+  @override
+  _CustomHideFieldState createState() => _CustomHideFieldState();
+}
+
+class _CustomHideFieldState extends State<CustomHideField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextFormField(
+          controller: widget.controller,
+          keyboardType: widget.keyboardType,
+          obscureText: _obscureText,
+          decoration: InputDecoration(
+            hintText: widget.hintText,
+            hintStyle: const TextStyle(
+              fontFamily: 'Nunito',
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+              color: GlobalVariables.grey,
+            ),
+            suffixIcon: widget.suffixIcon != null
+                ? IconButton(
+              icon: widget.suffixIcon!,
+              onPressed: widget.onSuffixIconPressed,
+            )
+                : IconButton(
+              icon: Icon(
+                _obscureText ? Icons.visibility : Icons.visibility_off,
+                color: GlobalVariables.appBarColor,
+              ),
+              onPressed: () {
+                setState(() {
+                  _obscureText = !_obscureText;
+                });
+              },
+            ),
+          ),
+          validator: widget.validator,
+        ),
+      ],
+    );
+  }
+}
