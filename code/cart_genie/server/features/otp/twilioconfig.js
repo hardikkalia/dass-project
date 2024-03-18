7d4561de824a999885c61fac919dec97892d11fa
UberMayinch <chinmay.sharma@research.iiit.ac.in>
2024-03-18 21:22:24 +0530

    functions for twilio verification

--- code/cart_genie/server/features/otp/twilioconfig.js
+++ code/cart_genie/server/features/otp/twilioconfig.js
@@ -1,8 +1,8 @@
-// const Twilio = require('twilio');
+ const Twilio = require('twilio');
 
-// const accountSid = '';
-// const authToken = '';
+ const accountSid = 'AC3ba918c0e97e76e16e878a77438d5fb9';
+ const authToken = '6e20cb6b151bb63c47bde28f17ca5b2f';
 
-// const twilioClient = new Twilio(accountSid, authToken);
+ const twilioClient = new Twilio(accountSid, authToken);
 
-// module.exports = twilioClient;
+ module.exports = twilioClient;
