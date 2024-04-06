da02e9588285c8478c2117b781339244fcf308b3
kayWHYdee <kushagradhingra1208@gmail.com>
2024-04-07 02:47:59 +0530

    error_

--- code/cart_genie/scraping/scrape_messages.dart
+++ code/cart_genie/scraping/scrape_messages.dart
@@ -1,56 +1,22 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:sms_advanced/sms_advanced.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() => runApp(MyApp());
+class SmsReaderService {
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SmsReaderScreen(),
-    );
-  }
-}
-
-class SmsReaderScreen extends StatefulWidget {
-  @override
-  SmsReaderScreenState createState() => SmsReaderScreenState();
-}
-
-class SmsReaderScreenState extends State<SmsReaderScreen> {
-  final SmsQuery query = SmsQuery();
-
-  @override
-  void initState() {
-    super.initState();
-    CheckPermissionsAndReadSms.perform(query);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('SMS Reader'),
-      ),
-      body: Center(
-        child: Text('Check console for output'),
-      ),
-    );
-  }
-}
-
-class CheckPermissionsAndReadSms {
-  static Future<void> perform(SmsQuery query) async {
+  Future<void> checkPermissionsAndReadSms() async {
     if (await Permission.sms.request().isGranted && await Permission.contacts.request().isGranted) {
-      DateTime lastUpdate = DateTime(2024, 4, 1); // Example date will be taken from the database where this will be stored
-      await _readSmsMessages(query, lastUpdate);
+      DateTime lastUpdate = DateTime(2024, 4, 1); // This could be fetched from preferences or your backend
+      await _readSmsMessages(lastUpdate);
     } else {
       print("Necessary permissions not granted");
     }
   }
 
-  static Future<void> _readSmsMessages(SmsQuery query, DateTime startDate) async {
+  Future<void> _readSmsMessages(DateTime startDate) async {
+    SmsQuery query = SmsQuery();
     List<SmsMessage> messages = await query.querySms(kinds: [SmsQueryKind.Inbox]);
 
     messages = messages.where((msg) {
@@ -64,9 +30,29 @@ class CheckPermissionsAndReadSms {
       return bDate.compareTo(aDate);
     });
 
-    for (var message in messages) {
-      print('Message from: ${message.address}, Date: ${message.dateSent}, Body: ${message.body}');
+    updateLastReadAndMessages(messages);
+  }
+
+  void updateLastReadAndMessages(List<SmsMessage> messages) async {
+    http.Response res = await http.post(
+      Uri.parse('$uri/??\\'),
+      body: jsonEncode({
+        'lastUpdate': DateTime.now().toString(),
+        'messages': messages.map((msg) => {
+          'content': msg.body,
+          'date': msg.dateSent,
+          'sender': msg.address,
+        }).toList(),
+      }),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (res.statusCode == 200) {
+      print('Messages and last update timestamp successfully updated.');
+    } else {
+      print('Failed to update messages and last update timestamp.');
     }
-    //update the lastUpdate again in mongodb
   }
 }
