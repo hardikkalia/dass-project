4cd8b2bee6d4fb61ce62e387d8044690d9c2c131
kayWHYdee <kushagradhingra1208@gmail.com>
2024-04-07 02:28:13 +0530

    update_scraping

--- code/cart_genie/scraping/scrape_messages.dart
+++ code/cart_genie/scraping/scrape_messages.dart
@@ -1,5 +1,3 @@
-// import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:sms_advanced/sms_advanced.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -17,68 +15,58 @@ class MyApp extends StatelessWidget {
 
 class SmsReaderScreen extends StatefulWidget {
   @override
-  _SmsReaderScreenState createState() => _SmsReaderScreenState();
+  SmsReaderScreenState createState() => SmsReaderScreenState();
 }
 
-class _SmsReaderScreenState extends State<SmsReaderScreen> {
+class SmsReaderScreenState extends State<SmsReaderScreen> {
   final SmsQuery query = SmsQuery();
 
   @override
   void initState() {
     super.initState();
-    _checkPermissionsAndReadSms();
-  }
-
-  void _checkPermissionsAndReadSms() async {
-  // final smsPermissionStatus = await Permission.sms.status;
-  // final contactsPermissionStatus = await Permission.contacts.status;
-  if (await Permission.sms.request().isGranted && await Permission.contacts.request().isGranted) {
-      _readSmsMessages();
-    }
-  // if (smsPermissionStatus.isGranted && contactsPermissionStatus.isGranted) {
-  //   _readSmsMessages();
-  // } else {
-  //   // ("Requesting permissions");
-  //   // final statuses = await [Permission.sms, Permission.contacts].request();
-  //   else {
-  //     // Handle the scenario where permissions are not granted
-  //     print("Necessary permissions not granted");
-  //   }
-  // }
-}
-
-  void _readSmsMessages() async 
-  {
-    // Fetch SMS messages that are in the inbox
-    List<SmsMessage> messages = await query.querySms(
-      kinds: [SmsQueryKind.Inbox],
-      // Assuming you want to fetch only the inbox messages sorted by date
-    );
-
-    // Sort messages by date if the API doesn't do it by default
-    messages.sort((a, b) {
-      // Treat null dates as the earliest possible date
-      var aDate = a.dateSent ?? DateTime.fromMillisecondsSinceEpoch(0);
-      var bDate = b.dateSent ?? DateTime.fromMillisecondsSinceEpoch(0);
-      return bDate.compareTo(aDate); // This ensures descending order; for ascending, swap aDate and bDate
-    });   
-    // Assuming you want to print or use the sorted messages
-    for (var message in messages) {
-      print('Message from: ${message.address}, Date: ${message.dateSent}, Body: ${message.body}');
-    }
-
-    // If you also want to work with threads, you can fetch and process them here
+    CheckPermissionsAndReadSms.perform(query);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('SMS Reader'),
+        title: const Text('SMS Reader'),
       ),
       body: Center(
         child: Text('Check console for output'),
       ),
     );
+  }
+}
+
+class CheckPermissionsAndReadSms {
+  static Future<void> perform(SmsQuery query) async {
+    if (await Permission.sms.request().isGranted && await Permission.contacts.request().isGranted) {
+      DateTime lastUpdate = DateTime(2024, 4, 1); // Example date will be taken from the database where this will be stored
+      await _readSmsMessages(query, lastUpdate);
+    } else {
+      print("Necessary permissions not granted");
+    }
+  }
+
+  static Future<void> _readSmsMessages(SmsQuery query, DateTime startDate) async {
+    List<SmsMessage> messages = await query.querySms(kinds: [SmsQueryKind.Inbox]);
+
+    messages = messages.where((msg) {
+      var msgDate = msg.dateSent ?? DateTime.fromMillisecondsSinceEpoch(0);
+      return msgDate.isAfter(startDate) || msgDate.isAtSameMomentAs(startDate);
+    }).toList();
+
+    messages.sort((a, b) {
+      var aDate = a.dateSent ?? DateTime.fromMillisecondsSinceEpoch(0);
+      var bDate = b.dateSent ?? DateTime.fromMillisecondsSinceEpoch(0);
+      return bDate.compareTo(aDate);
+    });
+
+    for (var message in messages) {
+      print('Message from: ${message.address}, Date: ${message.dateSent}, Body: ${message.body}');
+    }
+    //update the lastUpdate again in mongodb
   }
 }
