c8f7c3b683773b3e8ecac87e4add83ce1578dd20
kayWHYdee <kushagradhingra1208@gmail.com>
2024-04-07 01:18:02 +0530

    minor_db_update

---
+++ code/cart_genie/server/models/user_messages.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  date: { type: Date, required: true },
+  sender: { type: String, required: true },
+});
+
+const userAllMessagesSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Assuming you have a User model to reference
+  messages: [messageSchema],
+  number_of_messages: { type: Number, required: true, default: 0 },
+  last_update: { type: Date, required: true },
+});
+
+const UserAllMessages = mongoose.model('UserAllMessages', userAllMessagesSchema);
+
+module.exports = UserAllMessages;
