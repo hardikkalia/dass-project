13ab83bb08756de7258b52a358975f456eeb22bc
UberMayinch <chinmay.sharma@research.iiit.ac.in>
2024-04-19 17:58:04 +0530

    modified create query

--- code/cart_genie/server/features/filter/createquery.js
+++ code/cart_genie/server/features/filter/createquery.js
@@ -2,18 +2,25 @@ function createQuery(queryParams) {
     const query = {};
     console.log(queryParams);
 
-    if (queryParams.date) {
-      query["orders.full_messages.date"] = queryParams.date;
+    if (queryParams.startdate) {
+      query["orders.full_messages.startdate"] = queryParams.startDate;
     }
+
+    if (queryParams.enddate) {
+      query["orders.full_messages.enddate"] = queryParams.endDate;
   
     if (queryParams.company) {
-      query["company_name"] = queryParams.company;
+      query["orders.company_name"] = queryParams.company;
     }
   
     if (queryParams.orderStatus) {
       query["orders.current_status"] = queryParams.orderStatus;
     }
   
+    if (queryParams.orderType) {
+      query["orders.type"] = queryParams.orderType;
+    }
+
     return query;
   }
 
