8c7c29b66221b54b0a4cc23651fd380ce38bb474
argamas23 <127509973+argamas23@users.noreply.github.com>
2024-04-21 03:38:20 +0530

    filter resizing

--- code/cart_genie/lib/features/search/widgets/filters.dart
+++ code/cart_genie/lib/features/search/widgets/filters.dart
@@ -3,78 +3,74 @@ import 'package:flutter/material.dart';
 class FilterSection extends StatefulWidget {
   final String heading;
   final List<String> options;
-  final ValueChanged<String> onSelect;
+  final Function(String) onSelect;
 
-  const FilterSection({super.key, 
+  const FilterSection({
     required this.heading,
     required this.options,
     required this.onSelect,
   });
 
   @override
-  FilterSectionState createState() => FilterSectionState();
+  _FilterSectionState createState() => _FilterSectionState();
 }
 
-class FilterSectionState extends State<FilterSection> {
-  late String selectedOption;
-
-  @override
-  void initState() {
-    super.initState();
-    // Initialize selectedOption with an empty value
-    selectedOption = '';
-  }
+class _FilterSectionState extends State<FilterSection> {
+  String selectedOption = '';
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          padding: const EdgeInsets.symmetric(vertical: 8.0,horizontal: 10),
           child: Text(
             widget.heading,
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+            style: TextStyle(
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
             ),
           ),
         ),
-        Wrap(
-          spacing: 8.0,
-          runSpacing: 8.0,
-          children: List.generate(widget.options.length, (index) {
-            final option = widget.options[index];
-            return FilterButton(
-              text: option,
-              isSelected: selectedOption == option,
-              onTap: () {
-                setState(() {
-                  if (selectedOption == option) {
-                    // If the same option is tapped again, deselect it
-                    selectedOption = '';
-                  } else {
-                    selectedOption = option;
-                  }
-                  // Notify parent widget about the selected option
-                  widget.onSelect(selectedOption);
-                });
-              },
-            );
-          }),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Wrap(
+            spacing: 8.0,
+            runSpacing: 8.0,
+            children: List.generate(widget.options.length, (index) {
+              final option = widget.options[index];
+              return FilterButton(
+                text: option,
+                isSelected: selectedOption == option,
+                onTap: () {
+                  setState(() {
+                    if (selectedOption == option) {
+                      // If the same option is tapped again, deselect it
+                      selectedOption = '';
+                    } else {
+                      selectedOption = option;
+                    }
+                    // Notify parent widget about the selected option
+                    widget.onSelect(selectedOption);
+                  });
+                },
+              );
+            }),
+          ),
         ),
       ],
     );
   }
 }
 
-
 class FilterButton extends StatelessWidget {
   final String text;
   final bool isSelected;
   final VoidCallback onTap;
 
-  const FilterButton({super.key, 
+  const FilterButton({
     required this.text,
     required this.isSelected,
     required this.onTap,
@@ -85,7 +81,7 @@ class FilterButton extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         decoration: BoxDecoration(
           color: isSelected ? Colors.blue : Colors.grey[300],
           borderRadius: BorderRadius.circular(20),
@@ -94,7 +90,8 @@ class FilterButton extends StatelessWidget {
           text,
           style: TextStyle(
             color: isSelected ? Colors.white : Colors.black,
-            fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+            fontWeight: isSelected ? FontWeight.bold : FontWeight.normal ,
+
           ),
         ),
       ),
